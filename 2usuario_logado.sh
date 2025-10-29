09c9fc7cb4954eb7fdfce29d20d4b5ed36bb306e
Elton <elton.felype.s.nascimento@gmail.com>
2025-10-29 09:53:41 -0300

    questão 2

---
+++ 2usuario_logado.sh
@@ -0,0 +1,5 @@
+touch "output/Usuario_$USER.txt"
+
+echo "Usuário Logado: $USER" >> "output/Usuario_$USER.txt"
+echo "Id do Usuário: $UID" >> "output/Usuario_$USER.txt"
+echo "Diretório Home: $HOME" >> "output/Usuario_$USER.txt"
